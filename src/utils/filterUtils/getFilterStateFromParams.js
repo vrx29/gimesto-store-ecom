9bd683dc41cdc8a23d48cc76a10f9a4e11deab77
Vineet Kumar <jammackp@gmail.com>
2022-03-18 16:54:59 +0530

    refactor: update getStateFromParams function changed if-else to switch

--- src/utils/filterUtils/getFilterStateFromParams.js
+++ src/utils/filterUtils/getFilterStateFromParams.js
@@ -3,37 +3,39 @@ import { initialFilterState } from "../../reducers/constants/initialFilterState"
 const paramsReducer = (filterState, currParam) => {
   const [key, val] = currParam;
 
-  if (key === "sortBy") {
-    return {
-      ...filterState,
-      sortBy: val,
-    };
-  }
-  if (key === "categories") {
-    return {
-      ...filterState,
-      categories: [...filterState.categories, val],
-    };
-  }
-  if (key === "rating") {
-    return {
-      ...filterState,
-      rating: Number(val),
-    };
-  }
-  if (key === "priceLow") {
-    return {
-      ...filterState,
-      priceLow: [...filterState.priceLow, Number(val)],
-    };
-  }
-  if (key === "priceHigh") {
-    return {
-      ...filterState,
-      priceHigh: [...filterState.priceHigh, Number(val)],
-    };
+  switch (key) {
+    case "sortBy":
+      return {
+        ...filterState,
+        sortBy: val,
+      };
+    case "categories":
+      return {
+        ...filterState,
+        categories: [...filterState.categories, val],
+      };
+    case "rating":
+      return {
+        ...filterState,
+        rating: Number(val),
+      };
+    case "priceLow":
+      return {
+        ...filterState,
+        priceLow: [...filterState.priceLow, Number(val)],
+      };
+    case "priceHigh":
+      return {
+        ...filterState,
+        priceHigh: [...filterState.priceHigh, Number(val)],
+      };
+    default:
+      return {
+        ...filterState,
+      };
   }
 };
+
 export const getFilterStateFromParams = (searchParams) => {
   return [...searchParams].reduce(paramsReducer, initialFilterState);
 };
